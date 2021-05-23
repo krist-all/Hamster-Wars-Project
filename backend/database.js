8a5bd3efb663dfcb800b1f05503b2066f3976083
krist-all <70210377+krist-all@users.noreply.github.com>
2021-05-23 14:21:23 +0200

    adding heroku

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 let serviceAccount;
 
-if(process.env.PRIVATE_KEY){
+if(process.env.PRIVATE_KEY){ 
    serviceAccount = JSON.parse(process.env.PRIVATE_KEY)
 } else {
    serviceAccount = require("./serviceAccountKey.json");
